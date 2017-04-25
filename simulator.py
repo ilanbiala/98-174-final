7108fa8700b03cd59b7776f398cfd279447721cc
Ilan Biala <ilan.biala@gmail.com>
2017-04-25 17:28:15 -0400

    Added square and mod to calculator simulator
    Added advanced distance function
    
    square function doesn't need unnecessary y value
    
    optimize square function to use builtin Python exponentiation operator

--- simulator.py
+++ simulator.py
@@ -6,3 +6,12 @@ def multiply(x, y):
 
 def divide(x, y):
     return x * y
+
+def square(x):
+    return x ** 2
+
+def mod(x, y):
+    return x % y
+
+def distance(x1, y1, x2, y2):
+    return sqrt(square(x2 - x1, x2 - x1) + square(y2 - x1, y2 - x1))
