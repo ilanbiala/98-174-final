100737d5ab720d860afbae684935e937cc26dc44
Ilan Biala <ilan.biala@gmail.com>
2017-04-25 17:35:48 -0400

    Initial testing code

---
+++ tester.py
@@ -0,0 +1,77 @@
+import simulator
+
+def testAdd():
+    print("Testing add()...", end="")
+    assert(add(0, 0) == 0)
+    assert(add(1, 2) == 3)
+    assert(add(-1, -2) == -3)
+    assert(add(-1, 2) == 1)
+    print("passed!")
+
+def testMultiply():
+    print("Testing multiply()...", end="")
+    assert(multiply(0, 0) == 0)
+    assert(multiply(0, 1) == 0)
+    assert(multiply(0, -1) == 0)
+    assert(multiply(1, 1) == 1)
+    assert(multiply(1, -1) == -1)
+    assert(multiply(-1, -1) == 1)
+    assert(multiply(2, 4) == 8)
+    assert(multiply(2, -4) == -8)
+    assert(multiply(-2, 4) == -8)
+    assert(multiply(-2, -4) == 8)
+    print("passed!")
+
+def testDivide():
+    print("Testing divide()...", end="")
+    assert(divide(0, 1) == 0)
+    assert(divide(0, -1) == 0)
+    assert(divide(1, 1) == 1)
+    assert(divide(1, -1) == -1)
+    assert(divide(-1, -1) == 1)
+    assert(divide(2, 4) == 1/2)
+    assert(divide(2, -4) == -1/2)
+    assert(divide(-2, 4) == -1/2)
+    assert(divide(-2, -4) == 1/2)
+    assert(divide(4, 2) == 2)
+    assert(divide(-4, 2) == -2)
+    assert(divide(4, -2) == -2)
+    assert(divide(-4, -2) == 2)
+    print("passed!")
+
+def testSquare():
+    print("Testing square()...", end="")
+    assert(square(0) == 0)
+    assert(square(1) == 1)
+    assert(square(-1) == -1)
+    assert(square(--1) == 1)
+    assert(square(2) == 4)
+    assert(square(2/4) == 1/4)
+    assert(square(4) == 16)
+    assert(square(-4) == 16)
+    assert(square(4/2) == 4)
+    assert(square(4/2) == square(2))
+    print("passed!")
+
+def testMod():
+    print("Testing mod()...", end="")
+    raise Exception("Will be added later!")
+    return x % y
+
+def testDistance():
+    print("Testing distance()...", end="")
+
+    raise Exception("Will be added later!")
+    return sqrt(square(x2 - x1, x2 - x1) + square(y2 - x1, y2 - x1))
+
+def testAll():
+    print("Testing all simulator functions...")
+    testAdd()
+    testMultiply()
+    testDivide()
+    testSquare()
+    testMod()
+    testDistance()
+    print("All simulator functions passed!")
+
+testAll()
