f40abe3eaefaf2e71f3a1037c6b848d065f75f7d
Ilan Biala <ilan.biala@gmail.com>
2017-04-25 17:26:54 -0400

    Simpulator implementation
    A bunch of functions: 4 basics, sqare, mod, distance
    This was tested! And it worked!

---
+++ simulator.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def square(x):
+    return x ** 2
+
+def mod(x, y):
+    return x % y
+
+def distance(x1, y1, x2, y2):
+    return sqrt(square(x2 - x1, x2 - x1) + square(y2 - x1, y2 - x1))
