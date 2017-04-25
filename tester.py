d7734c0e0f007655014f59162a25e206f8f12ff2
Ilan Biala <ilan.biala@gmail.com>
2017-04-25 17:44:16 -0400

    Squashed testing code commits

--- tester.py
+++ tester.py
@@ -1,45 +1,88 @@
-def testAdd(x, y):
+from simulator import *
+
+def testAdd():
     print("Testing add()...", end="")
-
+    assert(add(0, 0) == 0)
+    assert(add(1, 2) == 3)
+    assert(add(-1, -2) == -3)
+    assert(add(-1, 2) == 1)
     print("passed!")
-    return x + y
 
-def testMultiply(x, y):
+def testMultiply():
     print("Testing multiply()...", end="")
-
+    assert(multiply(0, 0) == 0)
+    assert(multiply(0, 1) == 0)
+    assert(multiply(0, -1) == 0)
+    assert(multiply(1, 1) == 1)
+    assert(multiply(1, -1) == -1)
+    assert(multiply(-1, -1) == 1)
+    assert(multiply(2, 4) == 8)
+    assert(multiply(2, -4) == -8)
+    assert(multiply(-2, 4) == -8)
+    assert(multiply(-2, -4) == 8)
     print("passed!")
-    return x / y
 
-def testDivide(x, y):
+def testDivide():
     print("Testing divide()...", end="")
-
+    assert(divide(0, 1) == 0)
+    assert(divide(0, -1) == 0)
+    assert(divide(1, 1) == 1)
+    assert(divide(1, -1) == -1)
+    assert(divide(-1, -1) == 1)
+    assert(divide(2, 4) == 1/2)
+    assert(divide(2, -4) == -1/2)
+    assert(divide(-2, 4) == -1/2)
+    assert(divide(-2, -4) == 1/2)
+    assert(divide(4, 2) == 2)
+    assert(divide(-4, 2) == -2)
+    assert(divide(4, -2) == -2)
+    assert(divide(-4, -2) == 2)
     print("passed!")
-    return x * y
 
-def testSquare(x):
+def testSquare():
     print("Testing square()...", end="")
-
+    assert(square(0) == 0)
+    assert(square(1) == 1)
+    assert(square(-1) == 1)
+    assert(square(--1) == 1)
+    assert(square(2) == 4)
+    assert(square(2/4) == 1/4)
+    assert(square(4) == 16)
+    assert(square(-4) == 16)
+    assert(square(4/2) == 4)
+    assert(square(4/2) == square(2))
     print("passed!")
-    return x ** 2
 
-def testMod(x, y):
+def testMod():
     print("Testing mod()...", end="")
-
+    assert(mod(0, 1) == 0)
+    assert(mod(0, -1) == 0)
+    assert(mod(1, 1) == 0)
+    assert(mod(1, -1) == 0)
+    assert(mod(-1, -1) == 0)
+    assert(mod(2, 4) == 2)
+    assert(mod(2, -4) == -2)
+    assert(mod(-2, 4) == 2)
     print("passed!")
-    return x % y
 
-def testDistance(x1, y1, x2, y2):
+def testDistance():
     print("Testing distance()...", end="")
-
+    assert(distance(0, 0, 1, 0) == 1)
+    assert(distance(0, 0, -1, 0) == 1)
+    assert(distance(3, 0, 0, 4) == 5)
     print("passed!")
-    return sqrt(square(x2 - x1, x2 - x1) + square(y2 - x1, y2 - x1))
 
 def testAll():
     print("Testing all simulator functions...")
-    testAdd()
-    testMultiply()
-    testDivide()
-    testSquare()
-    testMod()
-    testDistance()
-    print("All simulator functions passed!")
+    try:
+        testAdd()
+        testMultiply()
+        testDivide()
+        testSquare()
+        testMod()
+        testDistance()
+    except Exception as e:
+        return False
+    else:
+        print("All simulator functions passed!")
+        return True
