b4dd19e61b6b50c6c5a2b5d1c0d1aea0a3199523
Ilan Biala <ilan.biala@gmail.com>
2017-04-25 17:31:06 -0400

    fixed tests, depolyment code
    Initial testing code
    
    Added tests for 4 function simulator calculator
    
    deleted some dead code
    
    Forgot to import simulator so functions are defined in tester
    
    Didn't call testAll() in tester.py, so nothing was actually being tested
    
    tester functions don't need any arguments
    
    Fixed simulator import, so now functions are actually defined
    
    Added tests and deployment code, fixed tests

--- simulator.py
+++ simulator.py
@@ -1,17 +1,19 @@
+import math
+
 def add(x, y):
     return x + y
 
 def multiply(x, y):
-    return x / y
-
-def divide(x, y):
     return x * y
 
+def divide(x, y):
+    return x / y
+
 def square(x):
-    return multiply(x, x)
+    return x ** 2
 
 def mod(x, y):
     return x % y
 
 def distance(x1, y1, x2, y2):
-    return sqrt(square(x2 - x1, x2 - x1) + square(y2 - x1, y2 - x1))
+    return math.sqrt(square(x2 - x1) + square(y2 - y1))
